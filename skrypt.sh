6e41ef2f06569bfc2e16ba4d72a2de3e363701b9
Maciej47857 <dsw47857@student.dsw.edu.pl>
2022-07-01 23:42:27 +0200

    obsługa liczby logów

--- skrypt.sh
+++ skrypt.sh
@@ -12,11 +12,26 @@ else
 :
 fi
 
-if [ "$1" = "--logs" ]; then
-for i in {001..100}
+if [ "$1" = "--logs" ] && [ -z "$2" ]; then
+for i in {1..100}
 						do
     							echo log${i} $0 '--date' $DATE > "log${i}.txt"
 						done					
+else
+:
+fi
+
+if [ "$1" = "--logs" ] && [ -n "$2" ]; then
+					
+j=1
+while [ $j -lt $2 ]
+do
+echo log${j} $0 '--logs' $2 $DATE > "log${j}.txt"
+(( j ++))
+done
+echo log${j} $0 '--logs' $2 $DATE > "log${j}.txt"
+
+
 else
 :
 fi
