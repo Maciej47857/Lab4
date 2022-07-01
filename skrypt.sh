66e6db92972ad49d9c998c4d08c8213c9775d701
Maciej47857 <dsw47857@student.dsw.edu.pl>
2022-07-01 23:34:10 +0200

    tworzy 100 logów

--- skrypt.sh
+++ skrypt.sh
@@ -7,7 +7,16 @@ date=$1
 DATE=`date +%d.%m.%y`
 
 if [ "$1" = "--date" ]; then
-echo $DATE					
+echo $DATE		
+else
+:
+fi
+
+if [ "$1" = "--logs" ]; then
+for i in {001..100}
+						do
+    							echo log${i} $0 '--date' $DATE > "log${i}.txt"
+						done					
 else
 :
 fi
